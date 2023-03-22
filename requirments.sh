311a498e8865fc09ac94366a8dfee3115ae0e321
ariel-zilber <workarikzil@gmail.com>
2023-03-22 23:47:41 +0200

    install docker section

--- requirments.sh
+++ requirments.sh
@@ -13,6 +13,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # install helm
 echo -e "${RED}===========[Install helm]========"
