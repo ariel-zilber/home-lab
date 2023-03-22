04c1930bff8513b55d7f6603e5e7a36b0291e874
ariel-zilber <workarikzil@gmail.com>
2023-03-22 23:36:23 +0200

    change permission

--- requirments.sh
+++ requirments.sh
@@ -4,11 +4,22 @@
 RED='\033[0;31m'
 
 
+# install docker
+echo -e "${RED}===========[Install docker]========"
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg lsb-release -y
+sudo mkdir -m 0755 -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
 
 # install helm
-echo -e "${RED}===========Install helm========"
+echo -e "${RED}===========[Install helm]========"
 curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
 sudo apt-get install apt-transport-https --yes
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
 sudo apt-get update
 sudo apt-get install helm -y
+
+#
