1d8fc2c20542b26c5664ba85421288fc9e09d836
ariel-zilber <workarikzil@gmail.com>
2023-03-22 23:29:27 +0200

    change permission

--- requirments.sh
+++ requirments.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+
+RED='\033[0;31m'
+
+
+
+# install helm
+echo -e "${RED}===========Install helm========"
+curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+sudo apt-get install apt-transport-https --yes
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update
+sudo apt-get install helm
